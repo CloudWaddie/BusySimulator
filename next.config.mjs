08fdf62e0796d647462f81aff912e5dae462c25f
cloudwaddie <25fazedw@cbc.sa.edu.au>
2025-08-10 16:38:54 +0930

    Refactor project structure and update configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true, // Optional
+    trailingSlash: true, // This is generally recommended for static exports like Github Pages for this insane project
+    output: 'export',
+    assetPrefix: isProd ? '/BusySimulator' : '',
+    basePath: isProd ? '/BusySimulator' : '',
+    images: {
+        unoptimized: true // Disable Next.js image optimization
+    },
+};
 
 export default nextConfig;
