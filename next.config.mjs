a8225255adbe7bda1c47060a4be17fa8beb36677
cloudwaddie <148834837+CloudWaddie@users.noreply.github.com>
2025-08-11 18:31:32 +0930

    Update layout and styles for Busy Simulator 2.0, including asset handling and header enhancements

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     reactStrictMode: true, // Optional
     trailingSlash: true, // This is generally recommended for static exports like Github Pages for this insane project
     output: 'export',
-    assetPrefix: isProd ? '/BusySimulator' : '',
-    basePath: isProd ? '/BusySimulator' : '',
+    basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
     images: {
         unoptimized: true // Disable Next.js image optimization
     },
